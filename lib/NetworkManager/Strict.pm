b611f701e02b9965c0201848270f1dcda1c1c1ad
rwp0 <rwp.primary@gmail.com>
2023-02-12 17:54:30 +0400

    pre-release

--- lib/NetworkManager/Strict.pm
+++ lib/NetworkManager/Strict.pm
@@ -1,3 +1,5 @@
+# ABSTRACT: NetworkManager strict settings class
+
 use v5.37.9;
 use experimental qw( class builtin try );
 use feature 'class';
@@ -10,8 +12,24 @@ class NetworkManager::Strict {
   field $path = Path::Tiny -> new( '/etc/NetworkManager/system-connections/' );
   field $ethernet = 'Wired connection 1';
   my %dns = (
-    OpenDNS => [ '208.67.222.123' , '208.67.220.123' ]
+    OpenDNS    => [ '208.67.222.123' , '208.67.220.123' ] ,
+    CloudFlare => ['1.1.1.3', '1.0.0.3'],
   );
+
+=head1 DESCRIPTION
+
+=head2 DNS
+
+=head3 OpenDNS
+
+https://www.opendns.com/setupguide/#familyshield
+
+=head3 CloudFlare
+
+https://blog.cloudflare.com/introducing-1-1-1-1-for-families/
+
+=cut
+
   method path ( ) { $path; }
   method ethernet ( ) { $ethernet; }
   method lock ( $profile = $ethernet ) {
